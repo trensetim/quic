9b689679db9280a2263c23fbaa079d4313f6e24a
Tim Trense <trensetim@gmail.com>
2020-11-05 19:14:28 +0100

    add package com.timtrense.quic.impl/package-info.java

---
+++ src/main/java/com/timtrense/quic/impl/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * QUIC implementation in pure Java.
+ *
+ * This package contains the implementation of {@link com.timtrense.quic}.
+ *
+ * @author Tim Trense
+ */
+package com.timtrense.quic.impl;
