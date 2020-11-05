74693511092259749640fdf994f9e6a16286efa9
Tim Trense <trensetim@gmail.com>
2020-11-05 18:42:11 +0100

    add package-info.java

---
+++ src/main/java/com/timtrense/quic/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * QUIC interface in pure java.
+ *
+ * This package contains a general purpose interface to the IETF QUIC transport protocol.
+ *
+ * @author Tim Trense
+ * @see <a href="https://tools.ietf.org/html/draft-ietf-quic-transport-32">QUIC Specification</a>
+ */
+package com.timtrense.quic;
