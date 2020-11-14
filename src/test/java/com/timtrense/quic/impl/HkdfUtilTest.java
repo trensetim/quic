e0c72fb78cab31c23d09c5c8dd0161198e071f4a
Tim Trense <trensetim@gmail.com>
2020-11-14 12:32:57 +0100

    added Tests for HkdfUtil to derive retry secret key and none

--- src/test/java/com/timtrense/quic/impl/HkdfUtilTest.java
+++ src/test/java/com/timtrense/quic/impl/HkdfUtilTest.java
@@ -1,9 +1,11 @@
 package com.timtrense.quic.impl;
 
+import at.favre.lib.crypto.HKDF;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -12,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 public class HkdfUtilTest {
 
     @Test
-    public void label_quic_hp_correctlyEncoded() {
+    public void label_quicHp_correctlyEncoded() {
         String decodedLabel = String.valueOf(
                 HkdfUtil.QUIC_LABEL_ENCODING_CHARSET.decode(
                         ByteBuffer.wrap(
@@ -24,7 +26,7 @@ public class HkdfUtilTest {
     }
 
     @Test
-    public void label_quic_iv_correctlyEncoded() {
+    public void label_quicIv_correctlyEncoded() {
         String decodedLabel = String.valueOf(
                 HkdfUtil.QUIC_LABEL_ENCODING_CHARSET.decode(
                         ByteBuffer.wrap(
@@ -36,7 +38,7 @@ public class HkdfUtilTest {
     }
 
     @Test
-    public void label_quic_key_correctlyEncoded() {
+    public void label_quicKey_correctlyEncoded() {
         String decodedLabel = String.valueOf(
                 HkdfUtil.QUIC_LABEL_ENCODING_CHARSET.decode(
                         ByteBuffer.wrap(
@@ -48,7 +50,7 @@ public class HkdfUtilTest {
     }
 
     @Test
-    public void label_quic_ku_correctlyEncoded() {
+    public void label_quicKu_correctlyEncoded() {
         String decodedLabel = String.valueOf(
                 HkdfUtil.QUIC_LABEL_ENCODING_CHARSET.decode(
                         ByteBuffer.wrap(
@@ -60,7 +62,7 @@ public class HkdfUtilTest {
     }
 
     @Test
-    public void label_client_in_correctlyEncoded() {
+    public void label_clientIn_correctlyEncoded() {
         String decodedLabel = String.valueOf(
                 HkdfUtil.QUIC_LABEL_ENCODING_CHARSET.decode(
                         ByteBuffer.wrap(
@@ -72,7 +74,7 @@ public class HkdfUtilTest {
     }
 
     @Test
-    public void label_server_in_correctlyEncoded() {
+    public void label_serverIn_correctlyEncoded() {
         String decodedLabel = String.valueOf(
                 HkdfUtil.QUIC_LABEL_ENCODING_CHARSET.decode(
                         ByteBuffer.wrap(
@@ -81,6 +83,24 @@ public class HkdfUtilTest {
                 )
         );
         assertEquals("server in", decodedLabel);
+    }
+
+    @Test
+    public void label_retrySecret_canBeDerivedFromSecretKeySecret() {
+        // see https://tools.ietf.org/html/draft-ietf-quic-tls-32#section-5.8
+        byte[] expanded = HkdfUtil.tlsExpandLabel(
+                HKDF.fromHmacSha256(), /*todo: is that hash correct?*/
+                HkdfUtil.QUIC_SECRET_KEY_SECRET, HkdfUtil.LABEL_QUIC_KEY, null, (128 / 8));
+        assertArrayEquals(HkdfUtil.QUIC_RETRY_SECRET_KEY, expanded);
+    }
+
+    @Test
+    public void label_retryNone_canBeDerivedFromSecretKeySecret() {
+        // see https://tools.ietf.org/html/draft-ietf-quic-tls-32#section-5.8
+        byte[] expanded = HkdfUtil.tlsExpandLabel(
+                HKDF.fromHmacSha256(), /*todo: is that hash correct?*/
+                HkdfUtil.QUIC_SECRET_KEY_SECRET, HkdfUtil.LABEL_QUIC_IV, null, (96 / 8));
+        assertArrayEquals(HkdfUtil.QUIC_RETRY_NONCE, expanded);
     }
 
 }
