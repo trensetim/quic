c340f273ea9ff0a3837398c3ab2a32e13b2a5187
Tim Trense <trensetim@gmail.com>
2020-11-14 14:54:46 +0100

    add ConnectionId.equalsValue to directly check equals for a byte-array serialized form

--- src/main/java/com/timtrense/quic/ConnectionId.java
+++ src/main/java/com/timtrense/quic/ConnectionId.java
@@ -1,11 +1,13 @@
 package com.timtrense.quic;
 
+import java.util.Arrays;
+
 /**
  * Each connection possesses a set of connection identifiers, or
  * connection IDs, each of which can identify the connection.
  * Connection IDs are independently selected by endpoints; each endpoint
  * selects the connection IDs that its peer uses.
- *
+ * <p>
  * The primary function of a connection ID is to ensure that changes in
  * addressing at lower protocol layers (UDP, IP) do not cause packets
  * for a QUIC connection to be delivered to the wrong endpoint.  Each
@@ -13,17 +15,17 @@ package com.timtrense.quic;
  * perhaps deployment-specific) method that will allow packets with that
  * connection ID to be routed back to the endpoint and to be identified
  * by the endpoint upon receipt.
- *
+ * <p>
  * Connection IDs MUST NOT contain any information that can be used by
  * an external observer (that is, one that does not cooperate with the
  * issuer) to correlate them with other connection IDs for the same
  * connection.  As a trivial example, this means the same connection ID
  * MUST NOT be issued more than once on the same connection.
- *
+ * <p>
  * Packets with long headers include Source Connection ID and
  * Destination Connection ID fields.  These fields are used to set the
  * connection IDs for new connections; see Section 7.2 for details.
- *
+ * <p>
  * Packets with short headers (Section 17.3) only include the
  * Destination Connection ID and omit the explicit length.  The length
  * of the Destination Connection ID field is expected to be known to
@@ -32,12 +34,12 @@ package com.timtrense.quic;
  * for connection IDs, or agree on an encoding scheme.  A fixed portion
  * could encode an explicit length, which allows the entire connection
  * ID to vary in length and still be used by the load balancer.
- *
+ * <p>
  * A Version Negotiation (Section 17.2.1) packet echoes the connection
  * IDs selected by the client, both to ensure correct routing toward the
  * client and to demonstrate that the packet is in response to a packet
  * sent by the client.
- *
+ * <p>
  * A zero-length connection ID can be used when a connection ID is not
  * needed to route to the correct endpoint.  However, multiplexing
  * connections on the same local IP address and port while using zero-
@@ -46,13 +48,13 @@ package com.timtrense.quic;
  * endpoint MUST NOT use the same IP address and port for multiple
  * connections with zero-length connection IDs, unless it is certain
  * that those protocol features are not in use.
- *
+ * <p>
  * When an endpoint uses a non-zero-length connection ID, it needs to
  * ensure that the peer has a supply of connection IDs from which to
  * choose for packets sent to the endpoint.  These connection IDs are
  * supplied by the endpoint using the NEW_CONNECTION_ID frame
  * (Section 19.15).
- *
+ * <p>
  * <p/>
  *
  * <h2>Issuing Connection IDs</h2>
@@ -64,7 +66,7 @@ package com.timtrense.quic;
  * initial connection ID is 0.  If the preferred_address transport
  * parameter is sent, the sequence number of the supplied connection ID
  * is 1.
- *
+ * <p>
  * Additional connection IDs are communicated to the peer using
  * NEW_CONNECTION_ID frames (Section 19.15).  The sequence number on
  * each newly issued connection ID MUST increase by 1.  The connection
@@ -72,7 +74,7 @@ package com.timtrense.quic;
  * connection ID provided by a Retry packet are not assigned sequence
  * numbers unless a server opts to retain them as its initial connection
  * ID.
- *
+ * <p>
  * When an endpoint issues a connection ID, it MUST accept packets that
  * carry this connection ID for the duration of the connection or until
  * its peer invalidates the connection ID via a RETIRE_CONNECTION_ID
@@ -81,7 +83,7 @@ package com.timtrense.quic;
  * use with the current connection at any time, in any packet type.
  * This includes the connection ID issued by the server via the
  * preferred_address transport parameter.
- *
+ * <p>
  * An endpoint SHOULD ensure that its peer has a sufficient number of
  * available and unused connection IDs.  Endpoints advertise the number
  * of active connection IDs they are willing to maintain using the
@@ -90,7 +92,7 @@ package com.timtrense.quic;
  * send connection IDs that temporarily exceed a peer's limit if the
  * NEW_CONNECTION_ID frame also requires the retirement of any excess,
  * by including a sufficiently large value in the Retire Prior To field.
- *
+ * <p>
  * A NEW_CONNECTION_ID frame might cause an endpoint to add some active
  * connection IDs and retire others based on the value of the Retire
  * Prior To field.  After processing a NEW_CONNECTION_ID frame and
@@ -98,7 +100,7 @@ package com.timtrense.quic;
  * connection IDs exceeds the value advertised in its
  * active_connection_id_limit transport parameter, an endpoint MUST
  * close the connection with an error of type CONNECTION_ID_LIMIT_ERROR.
- *
+ * <p>
  * An endpoint SHOULD supply a new connection ID when the peer retires a
  * connection ID.  If an endpoint provided fewer connection IDs than the
  * peer's active_connection_id_limit, it MAY supply a new connection ID
@@ -109,20 +111,20 @@ package com.timtrense.quic;
  * connection IDs to reduce the amount of per-path state it maintains,
  * such as path validation status, as its peer might interact with it
  * over as many paths as there are issued connection IDs.
- *
+ * <p>
  * An endpoint that initiates migration and requires non-zero-length
  * connection IDs SHOULD ensure that the pool of connection IDs
  * available to its peer allows the peer to use a new connection ID on
  * migration, as the peer will be unable to respond if the pool is
  * exhausted.
- *
+ * <p>
  * <p/>
  * <h1>Consuming and Retiring Connection IDs</h1>
  * An endpoint can change the connection ID it uses for a peer to
  * another available one at any time during the connection.  An endpoint
  * consumes connection IDs in response to a migrating peer; see
  * Section 9.5 for more.
- *
+ * <p>
  * An endpoint maintains a set of connection IDs received from its peer,
  * any of which it can use when sending packets.  When the endpoint
  * wishes to remove a connection ID from use, it sends a
@@ -130,13 +132,13 @@ package com.timtrense.quic;
  * RETIRE_CONNECTION_ID frame indicates that the connection ID will not
  * be used again and requests that the peer replace it with a new
  * connection ID using a NEW_CONNECTION_ID frame.
- *
+ * <p>
  * As discussed in Section 9.5, endpoints limit the use of a connection
  * ID to packets sent from a single local address to a single
  * destination address.  Endpoints SHOULD retire connection IDs when
  * they are no longer actively using either the local or destination
  * address for which the connection ID was used.
- *
+ * <p>
  * An endpoint might need to stop accepting previously issued connection
  * IDs in certain circumstances.  Such an endpoint can cause its peer to
  * retire connection IDs by sending a NEW_CONNECTION_ID frame with an
@@ -144,7 +146,7 @@ package com.timtrense.quic;
  * accept the previously issued connection IDs until they are retired by
  * the peer.  If the endpoint can no longer process the indicated
  * connection IDs, it MAY close the connection.
- *
+ * <p>
  * Upon receipt of an increased Retire Prior To field, the peer MUST
  * stop using the corresponding connection IDs and retire them with
  * RETIRE_CONNECTION_ID frames before adding the newly provided
@@ -156,7 +158,7 @@ package com.timtrense.quic;
  * connection IDs when requested can result in connection failures, as
  * the issuing endpoint might be unable to continue using the connection
  * IDs with the active connection.
- *
+ * <p>
  * An endpoint SHOULD limit the number of connection IDs it has retired
  * locally and have not yet been acknowledged.  An endpoint SHOULD allow
  * for sending and tracking a number of RETIRE_CONNECTION_ID frames of
@@ -164,7 +166,7 @@ package com.timtrense.quic;
  * forget a connection ID without retiring it, though it MAY choose to
  * treat having connection IDs in need of retirement that exceed this
  * limit as a connection error of type CONNECTION_ID_LIMIT_ERROR.
- *
+ * <p>
  * Endpoints SHOULD NOT issue updates of the Retire Prior To field
  * before receiving RETIRE_CONNECTION_ID frames that retire all
  * connection IDs indicated by the previous Retire Prior To value.
@@ -180,6 +182,16 @@ public interface ConnectionId {
      * @return the value of this streams id, non-negative
      */
     byte[] getValue();
+
+    /**
+     * Checks for {@link java.util.Arrays#equals(byte[], byte[])} with {@link #getValue()}
+     *
+     * @param value the value to compare this value with
+     * @return true of this connection id serializes to the given value
+     */
+    default boolean equalsValue( byte[] value ) {
+        return Arrays.equals( getValue(), value );
+    }
 
     /**
      * Each Connection ID has an associated sequence number to assist in
