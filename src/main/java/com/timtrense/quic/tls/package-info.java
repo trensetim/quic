01725a487e2de8db7490557c21c92d0407be4f75
Tim Trense <trensetim@gmail.com>
2020-11-18 17:40:44 +0100

    started the TLS 1.3 implementation for QUIC
    After searching tremendous amounts of time, I couldn't find any implementation of TLS 1.3 that was fit for use in this QUIC implementations.
    
    I considered:
    - Java SSE: Was to narrow to TCP and couldn't be easily adopted
    - Bouncy Castle: Would have had dangling functions if integrated into QUIC
    - kwik TLS: The author of the only other known QUIC implementation in Java provided a QUIC-specific TLS 1.3 implementation in compiled form. Due to it not being documented and not being open sourced under approachable licence, it is no option.
    
    Thus leaving me to have to implement TLS 1.3 for QUIC on my own

---
+++ src/main/java/com/timtrense/quic/tls/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the implementation of Transport Layer Security (TLS) Version 1.3
+ * as described in https://tools.ietf.org/html/rfc8446 .
+ */
+package com.timtrense.quic.tls;
