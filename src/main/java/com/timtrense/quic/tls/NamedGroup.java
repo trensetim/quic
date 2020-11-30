4b225e5b649ce2b2b865a7a442750f9e5f6d3bb3
Tim Trense <trensetim@gmail.com>
2020-11-30 18:05:46 +0100

    sonarqube enum constant naming convention

--- src/main/java/com/timtrense/quic/tls/NamedGroup.java
+++ src/main/java/com/timtrense/quic/tls/NamedGroup.java
@@ -59,22 +59,22 @@ import lombok.Getter;
 public enum NamedGroup {
 
     /* Elliptic Curve Groups (ECDHE) */
-    secp256r1( 0x0017 ),
-    secp384r1( 0x0018 ),
-    secp521r1( 0x0019 ),
-    x25519( 0x001D ),
-    x448( 0x001E ),
+    SECP256R1( 0x0017 ),
+    SECP384R1( 0x0018 ),
+    SECP521R1( 0x0019 ),
+    X25519( 0x001D ),
+    X448( 0x001E ),
 
     /* Finite Field Groups (DHE) */
-    ffdhe2048( 0x0100 ),
-    ffdhe3072( 0x0101 ),
-    ffdhe4096( 0x0102 ),
-    ffdhe6144( 0x0103 ),
-    ffdhe8192( 0x0104 ),
+    FFDHE2048( 0x0100 ),
+    FFDHE3072( 0x0101 ),
+    FFDHE4096( 0x0102 ),
+    FFDHE6144( 0x0103 ),
+    FFDHE8192( 0x0104 ),
 
     /* Reserved Code Points */
-    //ffdhe_private_use(0x01FC..0x01FF),
-    //ecdhe_private_use(0xFE00..0xFEFF)
+    //FFDHE_PRIVATE_USE(0x01FC..0x01FF),
+    //ECDHE_PRIVATE_USE(0xFE00..0xFEFF)
 
     // HIGHEST_VALUE( 0xFFFF )
     ;
